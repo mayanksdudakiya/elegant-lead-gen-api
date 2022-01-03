b15cec620a58ad792052f1881d96927d2605d3d2
Mayank Dudakiya <mayanksdudakiya@gmail.com>
2022-01-03 19:26:34 +0530

    Plugin initiated

---
+++ elegant-lead-gen-api.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Plugin Name: Elegant Lead Gen API
+ * Plugin URI: https://www.elegantthemes.com/plugins/elegant-lead-gen-api
+ * Description: Connect the Laravel application with WordPress
+ * Version: 1.0.0
+ * Author: Mayank Dudakiya
+ * Author URI: https://www.elegantthemes.com
+ * License: GPL3.0
+ * Text Domain: elegant-lead-gen
+ */
+
+if ( ! defined( 'ABSPATH' ) ) :
+	exit; // Exit if accessed directly.
+endif;
